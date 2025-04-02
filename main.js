a13442d9a5682df3ef214c6b7f858e6ae5032a83
bubblegum-pop <201892308+bubblegum-pop@users.noreply.github.com>
2025-04-02 15:32:44 -0700

    Create function CreateNewRow()

--- main.js
+++ main.js
@@ -3,8 +3,21 @@ Create and add a new div
 */
 const container = document.querySelector(".container");
 
-function createNewSquare() {
+function createNewSquare(rowElement = container) {
   let newSquare = document.createElement("div");
   newSquare.classList.add("square");
-  container.appendChild(newSquare);
+  rowElement.appendChild(newSquare);
+  return newSquare;
 }
+
+function createNewRow(numSquares) {
+  let newRow = document.createElement("div");
+  newRow.classList.add("row");
+  for (let i = 0; i < numSquares; i++) {
+    let newSquare = createNewSquare(newRow);
+    newSquare.style.marginLeft = "-1px";
+  }
+  container.appendChild(newRow);
+}
+
+createNewRow(16);

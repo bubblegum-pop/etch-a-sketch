936b7ad8c17ec8f93004f492bd229379f3186d9c
bubblegum-pop <201892308+bubblegum-pop@users.noreply.github.com>
2025-04-02 12:55:22 -0700

    Add square class to newSquare div and move styling to style.css

--- main.js
+++ main.js
@@ -2,12 +2,9 @@
 Create and add a new div
 */
 const container = document.querySelector(".container");
-const newSquare = document.createElement("div");
 
 function createNewSquare() {
   let newSquare = document.createElement("div");
-  newSquare.style.border = "1px solid black";
-  newSquare.style.height = "20px";
-  newSquare.style.width = "20px";
+  newSquare.classList.add("square");
   container.appendChild(newSquare);
 }

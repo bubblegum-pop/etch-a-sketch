1cb2a72d4326321753a1aa396f8878e96a3a7fea
bubblegum-pop <201892308+bubblegum-pop@users.noreply.github.com>
2025-04-03 14:11:14 -0700

    Create function removeAllChildren()

--- main.js
+++ main.js
@@ -19,6 +19,12 @@ function getGridSize() {
   return parseInt(prompt("Enter the number of squares per side:"));
 }
 
+function removeAllChildren(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
 const mainContainer = document.querySelector(".container");
 
 // Button
@@ -31,15 +37,23 @@ mainContainer.appendChild(gridSizeBtn);
 const gridContainer = document.createElement("div");
 gridContainer.classList.add("gridContainer");
 mainContainer.appendChild(gridContainer);
-
+/*
 // Create 16x16 grid
 for (let i = 0; i < 16; i++) {
   createNewRow(16, gridContainer);
 }
-
+*/
 const allSquares = document.querySelectorAll(".square");
 allSquares.forEach(square => {
   square.addEventListener("mouseover", e => {
     e.target.classList.add("filled");
   });
+});
+
+gridSizeBtn.addEventListener("click", () => {
+  removeAllChildren(gridContainer);
+  let gridSize = getGridSize();
+  for (let i = 0; i < gridSize; i++) {
+    createNewRow(gridSize, gridContainer);
+  }
 });
